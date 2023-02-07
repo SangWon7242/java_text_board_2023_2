773a4334e541f9f662f2edf7ec661fe78e9e8692
SangWon7242 <gotkdqja@gmail.com>
2023-02-07 20:26:18 +0900

    입력받은 게시물 정보를 Article 객체에 저장 후, 객체변수 출력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -24,10 +24,16 @@ public class Main {
         System.out.printf("내용 : ");
         String body = sc.nextLine();
         int id = articleLastId + 1;
-        // int id = ++articleLastId;
         articleLastId++;
-        // articleLastId = id
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+
+        System.out.println("생성 된 게시물 객체 : " + article);
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
       else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
@@ -37,4 +43,10 @@ public class Main {
     System.out.println("== 프로그램 종료 == ");
     sc.close();
   }
+}
+
+class Article {
+  int id;
+  String title;
+  String body;
 }
